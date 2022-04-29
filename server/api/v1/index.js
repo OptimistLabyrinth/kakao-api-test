3d1408d084d855c38a5e9bba4dc4b485d3387978
OptimistLabyrinth <keesung0705@gmail.com>
2022-04-29 11:28:22 +0900

    server for kakao login (Javascript SDK & REST API)

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const router = express.Router()
+const router = express.Router({ mergeParams: true })
 
-/* GET home page. */
 router.get('/', (req, res) => {
   res.send({ msg: 'REST API VERSION 1' })
 })
+
+const kakaoRouter = require('./kakao')
+router.use('/kakao', kakaoRouter)
 
 module.exports = router
--- server/app.js
+++ server/app.js
@@ -1,11 +1,18 @@
 const path = require('path')
 
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const express = require('express')
 const logger = require('morgan')
 
 const app = express()
 
+app.use(
+  cors({
+    origin: 'http://localhost:3050',
+    methods: '*',
+  })
+)
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -14,7 +21,7 @@ app.use(express.static(path.resolve('public')))
 
 const v1Router = require('./api/v1/index')
 
-app.use('/api', v1Router)
+app.use('/api/v1', v1Router)
 app.use('*', (req, res) => {
   res.redirect('/')
 })
